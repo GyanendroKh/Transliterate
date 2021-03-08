8e0cd26e3cd2447787ceaa57fdf02ba890f67342
Gyanendro Kh <khgyanendro77@gmail.com>
2021-03-08 10:20:00 +0530

    Passing command line arguments in gRPC client.

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
 import grpc
+import click
+from click import echo
 
 from rpc import service_pb2_grpc, service_pb2
 
 
-def run():
+@click.command()
+@click.option('--to', '-t', required=True, help='Language to transliterate to.')
+@click.argument('sentence')
+def run(to, sentence):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = service_pb2_grpc.TransStub(channel)
-        response = stub.infer(service_pb2.SourceWord(word='Helloo', to='mm'))
-    print(response.output)
-    print(response.time)
+        response = stub.infer(service_pb2.SourceWord(word=sentence, to=to))
+    echo(response.output)
+    echo(response.time)
 
 
 if __name__ == '__main__':
