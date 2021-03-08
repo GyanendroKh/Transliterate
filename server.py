5771a91c5dabc8eaf8dbf2011e7a38e3cdca4184
Gyanendro Kh <khgyanendro77@gmail.com>
2021-03-08 11:14:56 +0530

    Implementing click for gRPC server.

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from concurrent import futures
+import click
 import grpc
+import signal
 from time import time
 import os
-
-from rpc import service_pb2, service_pb2_grpc
 from utils import check_checkpoint_config
-from transformer.model import Model
+from rpc import service_pb2, service_pb2_grpc
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # noqa
+
+from transformer.model import Model  # noqa
 
 
 class Transliterator(service_pb2_grpc.TransServicer):
@@ -21,8 +25,9 @@ class Transliterator(service_pb2_grpc.TransServicer):
         return service_pb2.Output(output=output, time=end)
 
 
-def serve():
-    checkpoint = './checkpoints'
+@click.command()
+@click.option('--checkpoint', default='./checkpoints', help='Path to checkpoints to restore model.')
+def serve(checkpoint):
     config = os.path.join(checkpoint, 'config.json')
     config = check_checkpoint_config(config)
 
@@ -34,6 +39,13 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     print('Server Started...')
+
+    def on_done(signum, frame):
+        print()
+        print('Stopping Server.')
+        server.stop(None)
+
+    signal.signal(signal.SIGINT, on_done)
     server.wait_for_termination()
 
 
